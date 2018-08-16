ad11fb85d1586c8b096c517dcf3f830181532902
zhuguangwei <zhuguangwei@huizhongcf.com>
2018-08-16 14:50:59 +0800

    放开注册中心

--- basic-microservice/src/main/java/com/hzcf/BasicApplication.java
+++ basic-microservice/src/main/java/com/hzcf/BasicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @MapperScan("com.hzcf.basic.mapper")//扫描：该包下相应的class,主要是MyBatis的持久化类.
 public class BasicApplication {
 	public static void main(String[] args) {
