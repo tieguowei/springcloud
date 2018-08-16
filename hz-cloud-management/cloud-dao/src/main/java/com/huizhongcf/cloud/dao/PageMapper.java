d4d034e02de9a9f772c2a424e16361f00c30727b
zhuguangwei <zhuguangwei@huizhongcf.com>
2018-08-16 14:28:14 +0800

    删除旧的代码

--- hz-cloud-management/cloud-dao/src/main/java/com/huizhongcf/cloud/dao/PageMapper.java
+++
@@ -1,7 +0,0 @@
-package com.huizhongcf.cloud.dao;
-
-import com.huizhongcf.cloud.model.Page;
-
-public interface PageMapper extends BaseMapper<Page>{
-	
-}
--- hz-cloud-management/cloud-dao/src/main/java/com/huizhongcf/cloud/dao/SystemDictMapper.java
+++
@@ -1,7 +0,0 @@
-package com.huizhongcf.cloud.dao;
-
-import com.huizhongcf.cloud.model.SystemDict;
-
-public interface SystemDictMapper extends BaseMapper<SystemDict>{
-
-}
